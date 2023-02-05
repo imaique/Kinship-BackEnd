9ffa6625120b0e533c05ef2170283db9ff90a309
Michael <osujim2012@gmail.com>
2023-02-05 07:09:58 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import base64
 from recognition import FaceRecognition
 from utils import getBytes
 face_recogniser = FaceRecognition()
-app = FastAPI()
 
-app.add_middleware(
+
+origins = ["*"]
+
+middleware = [
+    Middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+)]
+
+app = FastAPI(middleware = middleware)
 
 
 class Picture(BaseModel):
